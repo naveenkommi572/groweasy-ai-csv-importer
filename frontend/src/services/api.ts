b52b252d01e880ccb525a8a39871cfc38e35a191
naveenkommi572 <naveenkommi572@gmail.com>
2026-07-13 20:07:40 +0530

    Use environment variable for backend API

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+const API =
+  process.env.NEXT_PUBLIC_API ||
+  "http://localhost:5000";
+
 export const importCSV = async (file: File) => {
-    const formData = new FormData();
+  const formData = new FormData();
 
-    formData.append("file", file);
+  formData.append("file", file);
 
-    const response = await axios.post(
-        "http://localhost:5000/api/import",
-        formData
-    );
+  const response = await axios.post(
+    `${API}/api/import`,
+    formData
+  );
 
-    return response.data;
+  return response.data;
 };
