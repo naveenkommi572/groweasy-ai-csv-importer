be0372e2c1009bb84a6df1cf9b5fff4d364da503
naveenkommi572 <naveenkommi572@gmail.com>
2026-07-13 20:56:58 +0530

    testing model

--- backend/src/services/aiService.ts
+++ backend/src/services/aiService.ts
@@ -2,7 +2,7 @@ import { crmPrompt } from "../prompts/crmPrompt";
 
 export async function mapCRM(records: any[]) {
   const MODEL = "models/gemini-2.5-flash";
-
+console.log("Using model:", MODEL);
   const response = await fetch(
     `https://generativelanguage.googleapis.com/v1beta/${MODEL}:generateContent`,
     {
